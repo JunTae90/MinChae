0dd2eae752050952b93c0ae5c7cbaea0d747320a
Lee <jt_lee@atectn.com>
2023-04-27 16:17:44 +0900

    ppt 생성까지

--- ppt.py
+++ ppt.py
@@ -1,15 +1,54 @@
 from PySide6.QtCore import *
 import re
+from pptx import Presentation
+from pptx.util import Inches, Cm
+from pptx.dml.color import *
+from PIL import Image
 class Maker(QThread):
+    start = Signal()
+    end = Signal
     def __init__(self, parent=None):
         super().__init__(parent)
         self.imgList = []
-
     def run(self):
-        for i in self.imgList:
-            print(i)
+        self.start.emit()
+        width = 25.4
+        height = 19.05
+        max_width = 25.4/3
+        max_height = 19.05/2
+        prs = Presentation()
+        prs.slide_width = Cm(width)
+        prs.slide_height = Cm(height)
+        slide_layout = prs.slide_layouts[6]
 
-    @Slot(str, result='QVariant')
+        for key in self.imgNames.keys():
+            imgNames = self.imgNames[key]
+            if len(imgNames) == 0:
+                continue
+            slide = prs.slides.add_slide(slide_layout)
+            tb = slide.shapes.add_textbox(Cm(0), Cm(0), Cm(width/10), Cm(height/20))
+            tf = tb.text_frame
+            tf.clear()
+            p = tf.paragraphs[0]
+            run = p.add_run()
+            run.text = key
+            font = run.font
+            font.color.rgb = RGBColor(255, 0, 0)
+            for i in range(0, len(imgNames)):
+                if i // 6 > 0 and i % 6 == 0:
+                    slide = prs.slides.add_slide(slide_layout)
+                im = Image.open(self.path+imgNames[i])
+                x = max_width*((i % 6) % 3)
+                y = max_height*((i % 6) // 3)
+                if im.size[0] > im.size[1]:
+                    pc = slide.shapes.add_picture(self.path+imgNames[i], Cm(x), Cm(y), width = Cm(max_width))
+                else:
+                    pc = slide.shapes.add_picture(self.path + imgNames[i], Cm(x), Cm(y), height=Cm(max_height))
+                slide.shapes._spTree.insert(2, pc._element)
+
+        prs.save(self.path+"demo.pptx")
+        self.end.emit()
+    @Slot(str, result=dict)
     def extractInfo(self, chatPath):
         res = {}
         cur = ""
@@ -37,7 +76,8 @@ class Maker(QThread):
     def check(self):
         return self.isRunning()
 
-    @Slot(list)
-    def startMake(self, list):
-        self.imgList = list
+    @Slot(str, dict)
+    def startMake(self, path, imgNames):
+        self.path = path
+        self.imgNames = imgNames
         self.start()
